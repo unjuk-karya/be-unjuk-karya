f7cb04f0a853294d2ec2ef3410e1dc05d0457e35
Krisna Diva <krisnadiva0412@gmail.com>
2024-11-26 09:41:04 +0700

    Menambahkan search user

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -4,8 +4,10 @@ const authRoutes = require('./auth.routes');
 const postRoutes = require('./post.routes');
 const commentRoutes = require('./comment.routes');
 const likeRoutes = require('./like.routes');
+const userRoutes = require('./user.routes'); 
 
 router.use('/auth', authRoutes);
+router.use('/users', userRoutes); 
 router.use('/posts', postRoutes);
 router.use('/posts/:postId/comments', commentRoutes);
 router.use('/posts/:postId/likes', likeRoutes);
